b115e9bdb24e7119e028dfdb6144995f4a9ce2fd
Dominic Parducci <dominic.parducci@Dominics-MBP.fios-router.home>
2019-08-28 11:27:27 -0700

    delete functionality

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const myList = [];
+var myList = [];
 
 window.onload = function() {
-  //   const row = document.createElement("tr");
-  const list = document.querySelector("#item-list");
-  var theString = JSON.parse(document.cookie);
-  console.log("T he String: ", theString);
-  for (i = 0; i < theString.length; i++) {
-    myList.push(theString[i]);
-    console.log("The String: ", theString);
-  }
-  for (i = 0; i < myList.length; i++) {
-    const row = document.createElement("tr");
+  console.log(document.cookie);
+  if (document.cookie) {
+    //   const row = document.createElement("tr");
+    const list = document.querySelector("#item-list");
+    var theString = JSON.parse(document.cookie);
+    //console.log("T he String: ", theString);
+    for (i = 0; i < theString.length; i++) {
+      myList.push(theString[i]);
+      //console.log("The String: ", theString);
+    }
+    for (i = 0; i < myList.length; i++) {
+      const row = document.createElement("tr");
 
-    row.innerHTML = `
-            <td>${myList[i].qty}<td>
-            <td>${myList[i].name}<td>
-            <td>${myList[i].units}<td>
-            <td>${(myList[i].completed = false)}<td>
-            <td><a href="#" class="btn btn-primary btn-sm edit" >Edit</a><td>
-            <td><a href="#" class="btn btn-danger btn-sm delete" id=d${i}>X</a><td>
-        `;
-    list.appendChild(row);
+      row.innerHTML = `
+                  <td>${myList[i].qty}<td>
+                  <td>${myList[i].name}<td>
+                  <td>${myList[i].units}<td>
+                  <td>${(myList[i].completed = false)}<td>
+                  <td><a href="#" class="btn btn-primary btn-sm edit" >Edit</a><td>
+                  <td><a href="#" class="btn btn-danger btn-sm delete" id=${i}>X</a><td>
+              `;
+      list.appendChild(row);
+    }
+    //console.log("MyList Here:", myList);
   }
-  console.log("MyList Here:", myList);
 };
 
 console.log(JSON.stringify(myList));
@@ -41,15 +44,13 @@ class userInt {
   static displayList() {
     const storedList = myList;
     const shoppinglist = storedList;
-    console.log("shopping", shoppinglist);
-    shoppinglist.forEach(function(item) {
-      console.log("foreach: ", item);
-    });
+    //console.log("shopping", shoppinglist);
+
     //shoppinglist.forEach(item => userInt.addItemToList(item));
   }
 
   static addItemToList(item) {
-    console.log(item);
+    //console.log(item);
     const list = document.querySelector("#item-list");
 
     const row = document.createElement("tr");
@@ -60,19 +61,16 @@ class userInt {
             <td>${item.units}<td>
             <td>${(item.completed = false)}<td>
             <td><a href="#" class="btn btn-primary btn-sm edit" id="edit">Edit</a><td>
-            <td><a href="#" class="btn btn-danger btn-sm delete" id=d${i}>X</a><td>
+            <td><a href="#" class="btn btn-danger btn-sm delete" id="">X</a><td>
         `;
     list.appendChild(row);
-    row.lastElementChild.addEventListener("click", function() {
-      console.log("please");
-    });
   }
 
-  //   static deleteItem(elem) {
-  //     if (elem.classList.contains("delete")) {
-  //       elem.parentElement.parentElement.remove();
-  //     }
-  //   }
+  static deleteItem(elem) {
+    if (elem.classList.contains("delete")) {
+      elem.parentElement.parentElement.remove();
+    }
+  }
 
   static editItem(elem) {
     if (elem.classList.contains("edit")) {
@@ -115,7 +113,6 @@ document.addEventListener(
 //Event: Add A List Item
 document.getElementById("list-form").addEventListener("submit", e => {
   e.preventDefault();
-  console.log("yep");
   const qty = document.querySelector("#quantity").value;
   const name = document.querySelector("#name").value;
   const unit = document.querySelector("#unit").value;
@@ -124,24 +121,21 @@ document.getElementById("list-form").addEventListener("submit", e => {
   const list = new List(qty, name, unit, completed);
   userInt.addItemToList(list);
   userInt.restoreFields();
-  console.log("the list:", list);
   myList.push(list);
-  console.log("my list: ", myList);
   const stringList = JSON.stringify(myList);
   document.cookie = stringList;
-  console.log("cookie:", document.cookie);
 });
-// Event: Remove Item
 
-// document.querySelectorAll("#item-list").addEventListener("click", e => {
-//   userInt.deleteItem(e.target);
-// });
-
-for (i = 0; i < myList.length; i++) {
-  document.getElementById(`d${i}`).addEventListener("click", function() {
-    console.log("this better fucking work");
-  });
-}
+// Delete Event
+document.querySelector("#item-list").addEventListener("click", e => {
+  console.log("MyList before anything deleted: ", myList);
+  let idx = e.target.id;
+  myList.splice(idx, 1);
+  console.log("THE INDEX", idx);
+  var jsonList = JSON.stringify(myList);
+  document.cookie = jsonList;
+  userInt.deleteItem(e.target);
+});
 
 document.querySelector("#item-list").addEventListener("click", e => {
   userInt.editItem(e.target);
